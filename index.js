cb6031c7192e86d58f2924ee542141e87d3ffbfa
Francis Chen <fncis.chen@gmail.com>
2019-03-19 13:51:49 -0700

    setting up outline for router

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const server = express();
 const router = express.Router(); 
 
 const data = require("./data/seeds/posts");
+
+router.get( (req, res) => {
+
+});
+
+router.get( (req, res) => {
+
+});
+
+router.post( (req, res) => {
+
+});
+
+router.delete( (req, res) => {
+
+});
+
+router.put( (req, res) => {
+
+});
