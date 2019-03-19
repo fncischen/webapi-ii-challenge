fd7219666e8fd56b8e7944922f76e5aca223ced2
Francis Chen <fncis.chen@gmail.com>
2019-03-19 14:48:35 -0700

    added delete post

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ router.get( "/api/posts", (req, res) => {
 
 router.get( "/api/posts:id", (req, res) => {
     if(!data.find(req.params.id)){
-        res.status(500).json({ error: "There was an error while saving the post to the database" })
+        res.status(404).json({ error: "The post information could not be retrieved."})
     }
     else {
         res.json(data.find(req.params.id));
@@ -26,13 +26,34 @@ router.post( "/api/posts", (req, res) => {
     if(!req.body.title | !req.body.contents) {
         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
     }
-    else if (req.body.title != null & req.body.contents != null) {
-        data.insert(req.body);
-        res.status(201).json(req.body);
+    else{ 
+
+        try{
+            data.insert(req.body);
+            res.status(201).json(req.body);
+        }
+        catch {
+            req.status(400).send({errorMessage: "A bad request was sent!"})
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
+        }
     }
 });
 
 router.delete( "/api/posts:id", (req, res) => {
+    if(!data.find(req.params.id)){
+        res.status(404).json({ error: "The post with the specified ID does not exist." })
+    }
+    else {
+
+        try{
+            data.delete(req.params.id);
+        }
+        catch {
+            req.status(400).send({errorMessage: "A bad request was sent!"})
+            res.status(500).json({ error: "The post could not be removed" });
+        }
+
+    }
 
 });
 
