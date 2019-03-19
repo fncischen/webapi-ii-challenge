5615040ec78441dc3328c9c80385290a60fd870d
Francis Chen <fncis.chen@gmail.com>
2019-03-19 15:40:46 -0700

    testing out server before doing router calls

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const router = express.Router();
 
 const data = require("./data/db.js");
 
-router.get( "/api/posts", (req, res) => {
-    console.log(data.find());
+
+server.get( "/api/posts", (req, res) => {
+    // console.log(data);
+    // console.log(data.find());
     if(!data.find()){
         res.status(500).json({ error: "There was an error while saving the post to the database" })
     }
     else {
-        res.json(data.find());
+        console.log("getting data!")
+        res.send(data.find());
     }
 });
 
-router.get( "/api/posts:id", (req, res) => {
+server.get( "/api/posts:id", (req, res) => {
     if(!data.findById(req.params.id)){
         res.status(404).json({ error: "The post information could not be retrieved."})
     }
@@ -23,7 +26,7 @@ router.get( "/api/posts:id", (req, res) => {
     }
 });
 
-router.post( "/api/posts", (req, res) => {
+server.post( "/api/posts", (req, res) => {
     if(!req.body.title | !req.body.contents) {
         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
     }
@@ -40,7 +43,7 @@ router.post( "/api/posts", (req, res) => {
     }
 });
 
-router.delete( "/api/posts:id", (req, res) => {
+server.delete( "/api/posts:id", (req, res) => {
     if(!data.findById(req.params.id)){
         res.status(404).json({ error: "The post with the specified ID does not exist." })
     }
@@ -58,7 +61,7 @@ router.delete( "/api/posts:id", (req, res) => {
 
 });
 
-router.put( "/api/posts:id", (req, res) => {
+server.put( "/api/posts:id", (req, res) => {
     if(!data.findById(req.params.id)){
         res.status(404).json({ error: "The post with the specified ID does not exist." })
     }
@@ -79,8 +82,10 @@ router.put( "/api/posts:id", (req, res) => {
     }
 });
 
-// server.use('/', (req, res) => res.send('API up and running!'));
-// server.use('/api/posts', (req, res) => res.json(req.body));
+// router.use("/posts", router);
+
+server.use('/', (req, res) => res.send('API up and running!'));
+// server.use('/api/posts', router);
 // server.use('/api/posts/:id', (req, res) => res.json(req.body));
 
 server.listen(5000, () =>
